corrected code errors: 1. Log Likelihood needs to be summed over the Y vector dimension. 2. Bernstein Layer Determinant of Jacobian did not include the normalizing of the input variable that is done prior. After including it the KL divergence in experiments makes sense.

bernstein_transformation_layer.py
@@ -44,7 +44,8 @@ def bernstein_prediction(params_a, input_a, degree, polynomial_range, monotonica
     first_order_ridge_pen = 0
     param_ridge_pen = 0
 
-    input_a = (input_a - polynomial_range[0]) / (polynomial_range[1] - polynomial_range[0])
+    normalizing_range = polynomial_range[1] - polynomial_range[0]
+    input_a = (input_a - polynomial_range[0]) / (normalizing_range)
 
     if derivativ == 0:
         output = sum(params_restricted[v] * b(torch.FloatTensor([v]), torch.FloatTensor([n]), input_a) for v in range(n+1)) #before we had: params_restricted[v-1]
@@ -53,8 +54,12 @@ def bernstein_prediction(params_a, input_a, degree, polynomial_range, monotonica
         #return (output + polynomial_range[0]) / (polynomial_range[1] - polynomial_range[0])
 
     elif derivativ == 1:
-        output = sum(params_restricted[v] * torch.FloatTensor([n]) * (b(torch.FloatTensor([v-1]), torch.FloatTensor([n-1]), input_a) -
-                                    b(torch.FloatTensor([v]), torch.FloatTensor([n-1]), input_a)) for v in range(n+1))
+        #output = sum(params_restricted[v] * b(torch.FloatTensor([v - 1]), torch.FloatTensor([n]), input_a) * (
+        #            torch.FloatTensor([v]) - torch.FloatTensor([n]) * input_a) for v in range(n + 1))
+        # My derivativ see goodnotes
+        output = 1/normalizing_range * sum(params_restricted[v] * torch.FloatTensor([n]) * (b(torch.FloatTensor([v-1]), torch.FloatTensor([n-1]), input_a) -
+                                                                                            b(torch.FloatTensor([v]), torch.FloatTensor([n-1]), input_a)) for v in range(n+1))
+        # The Bernstein polynomial basis: A centennial retrospective p.391 (17)
 
         return output, second_order_ridge_pen, first_order_ridge_pen, param_ridge_pen
         #return (output + polynomial_range[0]) / (polynomial_range[1] - polynomial_range[0])
@@ -104,7 +109,8 @@ class Transformation(nn.Module):
         if not inverse:
             output, second_order_ridge_pen_sum, first_order_ridge_pen_sum, param_ridge_pen_sum = multivariable_bernstein_prediction(input, self.degree, self.number_variables, self.params, self.polynomial_range, monotonically_increasing)
             output_first_derivativ, second_order_ridge_pen, first_order_ridge_pen, param_ridge_pen = multivariable_bernstein_prediction(input, self.degree, self.number_variables, self.params, self.polynomial_range, monotonically_increasing, derivativ=1)
-            log_d = log_d + torch.log(torch.abs(output_first_derivativ)) # Error this is false we require the derivativ of the bernstein polynomial!332'
+            log_d = log_d + torch.log(output_first_derivativ) # Error this is false we require the derivativ of the bernstein polynomial!332'
+            # took out torch.abs(), misunderstanding, determinant can be a negativ value (flipping of the coordinate system)
         else:
             output = multivariable_bernstein_prediction(input, self.degree, self.number_variables, self.params_inverse, self.polynomial_range_inverse, monotonically_increasing=False)
 

nf_mctm.py
@@ -79,5 +79,6 @@ class NF_MCTM(nn.Module):
     def log_likelihood(self, y):
         z, log_d, second_order_ridge_pen_global, first_order_ridge_pen_global, param_ridge_pen_global = self.forward(y,train=True)
         log_likelihood_latent = torch.distributions.Normal(0, 1).log_prob(z)  # log p_source(z)
-        log_likelihood = log_likelihood_latent + log_d
-        return log_likelihood
+        log_likelihood = log_likelihood_latent + log_d #now a minus here
+        vec_log_likelihood = log_likelihood.sum(1)
+        return vec_log_likelihood